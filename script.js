4d6947615fb46b4c930416ea090ccdf391f0fbe0
haruki-max <dtrgkt4n8k@privaterelay.appleid.com>
2026-06-14 21:49:15 +0900

    通知機能

--- script.js
+++ script.js
@@ -47,15 +47,20 @@ function addSubject() {
     const memo = prompt("メモ");
     const risk = prompt("危険 / 普通 / 安全");
 
+    const task = prompt("課題名（なければ空白）");
+    const deadline = prompt("締切（例: 2026-06-20 18:00）");
+
     subjects.push({
-        name,
-        teacher,
-        credit,
-        attendance,
-        test,
-        report,
-        memo,
-        risk
+    name,
+    teacher,
+    credit,
+    attendance,
+    test,
+    report,
+    memo,
+    risk,
+    task,
+    deadline
     });
 
     saveSubjects();
@@ -92,6 +97,8 @@ list.sort((a, b) => {
 <p>出席：${subject.attendance}</p>
 <p>テスト：${subject.test}</p>
 <p>レポート：${subject.report}</p>
+<p>課題：${subject.task || "なし"}</p>
+<p>締切：${subject.deadline || "なし"}</p>
 <p>メモ：${subject.memo || "なし"}</p>
 
 <span class="badge">${subject.risk}</span>
@@ -125,3 +132,78 @@ searchBox.addEventListener("input", function () {
 });
 
 displaySubjects(subjects);
+
+// 通知許可
+if (Notification.permission !== "granted") {
+  Notification.requestPermission();
+}
+
+// 締切チェック
+function checkDeadlines() {
+  const now = new Date();
+
+  subjects.forEach(subject => {
+    if (!subject.deadline || subject.deadline === "なし") return;
+
+    const deadlineDate = new Date(subject.deadline);
+    const diffTime = deadlineDate - now;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    // 明日締切
+    if (diffDays === 1) {
+      new Notification("⚠️課題締切が近い", {
+        body: `${subject.name} の課題が明日締切！`
+      });
+    }
+
+    // 今日締切
+    if (diffDays === 0) {
+      new Notification("🚨今日締切", {
+        body: `${subject.name} の課題今日まで！`
+      });
+    }
+  });
+}
+
+// 10秒後に確認（テスト用）
+setInterval(checkDeadlines, 3600000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("notifyBtn").addEventListener("click", function () {
+
+        alert("押された");
+
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                new Notification("通知テスト", {
+                    body: "通知が有効になった！"
+                });
+            }
+        });
+
+    });
+});
+
+function deleteSubject(subjectName) {
+    subjects = subjects.filter(subject => subject.name !== subjectName);
+
+    localStorage.setItem("subjects", JSON.stringify(subjects));
+
+    displaySubjects(subjects);
+}
+
+function editSubject(subjectName) {
+    const subject = subjects.find(subject => subject.name === subjectName);
+
+    if (!subject) return;
+
+    const newDeadline = prompt("新しい締切を入力", subject.deadline);
+
+    if (newDeadline !== null) {
+        subject.deadline = newDeadline;
+
+        localStorage.setItem("subjects", JSON.stringify(subjects));
+
+        displaySubjects(subjects);
+    }
+}
